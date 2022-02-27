f8bedd76775f363142cca3c6144aa3d655b48de0
Andy Arthur <andy@dolthub.com>
2022-02-26 22:53:53 -0800

    open file store as append only, update file position after root write

--- go/store/chunks/file_store.go
+++ go/store/chunks/file_store.go
@@ -31,13 +31,20 @@ const (
 	fileStoreName = "chunk.store"
 
 	defaultIndexSz = 1024
-	defaultBuffSz  = 1024 * 16
+	defaultBuffSz  = 1024 * 1024
 )
 
+var store *FileStore
+
 func NewFileStore(path, ver string) (*FileStore, error) {
+	// TODO(andy): this is gross and bad
+	if store != nil {
+		return store, nil
+	}
+
 	path = filepath.Join(path, fileStoreName)
 
-	flag := os.O_CREATE | os.O_RDWR
+	flag := os.O_CREATE | os.O_RDWR | os.O_APPEND
 	perm := os.ModePerm | os.ModeExclusive
 
 	f, err := os.OpenFile(path, flag, perm)
@@ -48,19 +55,19 @@ func NewFileStore(path, ver string) (*FileStore, error) {
 	wr := bufio.NewWriterSize(f, defaultBuffSz)
 	idx := make(map[hash.Hash]indexEntry, defaultIndexSz)
 
-	fs := &FileStore{
+	store = &FileStore{
 		idx: idx,
-		rd:  f,
+		f:   f,
 		wr:  wr,
 		mu:  &sync.RWMutex{},
 		ver: ver,
 	}
 
-	if err = populateFileStoreIndex(fs); err != nil {
+	if err = populateFileStoreIndex(store); err != nil {
 		return nil, err
 	}
 
-	return fs, nil
+	return store, nil
 }
 
 type FileStore struct {
@@ -68,7 +75,7 @@ type FileStore struct {
 	idx  map[hash.Hash]indexEntry
 	mu   *sync.RWMutex
 
-	rd  io.ReaderAt
+	f   *os.File
 	wr  *bufio.Writer
 	pos int64
 
@@ -83,29 +90,29 @@ type indexEntry struct {
 	offset uint32
 }
 
-func (fs *FileStore) Get(ctx context.Context, h hash.Hash) (Chunk, error) {
-	fs.mu.RLock()
-	defer fs.mu.RUnlock()
+func (st *FileStore) Get(ctx context.Context, h hash.Hash) (Chunk, error) {
+	st.mu.RLock()
+	defer st.mu.RUnlock()
 
-	e, ok := fs.idx[h]
+	e, ok := st.idx[h]
 	if !ok {
 		return EmptyChunk, nil
 	}
 
-	return fs.readChunk(e)
+	return st.readChunk(e)
 }
 
-func (fs *FileStore) GetMany(ctx context.Context, hashes hash.HashSet, found func(context.Context, *Chunk)) error {
-	fs.mu.RLock()
-	defer fs.mu.RUnlock()
+func (st *FileStore) GetMany(ctx context.Context, hashes hash.HashSet, found func(context.Context, *Chunk)) error {
+	st.mu.RLock()
+	defer st.mu.RUnlock()
 
 	for h := range hashes {
-		e, ok := fs.idx[h]
+		e, ok := st.idx[h]
 		if !ok {
 			continue
 		}
 
-		c, err := fs.readChunk(e)
+		c, err := st.readChunk(e)
 		if err != nil {
 			return err
 		}
@@ -115,83 +122,85 @@ func (fs *FileStore) GetMany(ctx context.Context, hashes hash.HashSet, found fun
 	return nil
 }
 
-func (fs *FileStore) Has(ctx context.Context, h hash.Hash) (ok bool, err error) {
-	fs.mu.RLock()
-	defer fs.mu.RUnlock()
+func (st *FileStore) Has(ctx context.Context, h hash.Hash) (ok bool, err error) {
+	st.mu.RLock()
+	defer st.mu.RUnlock()
 
-	_, ok = fs.idx[h]
+	_, ok = st.idx[h]
 	return
 }
 
-func (fs *FileStore) HasMany(ctx context.Context, hashes hash.HashSet) (absent hash.HashSet, err error) {
-	fs.mu.RLock()
-	defer fs.mu.RUnlock()
+func (st *FileStore) HasMany(ctx context.Context, hashes hash.HashSet) (absent hash.HashSet, err error) {
+	st.mu.RLock()
+	defer st.mu.RUnlock()
 
 	absent = hash.NewHashSet()
 	for h := range hashes {
-		if _, ok := fs.idx[h]; !ok {
+		if _, ok := st.idx[h]; !ok {
 			absent.Insert(h)
 		}
 	}
 	return
 }
 
-func (fs *FileStore) Put(ctx context.Context, c Chunk) error {
-	fs.mu.Lock()
-	defer fs.mu.Unlock()
-	return fs.writeChunk(c)
+func (st *FileStore) Put(ctx context.Context, c Chunk) error {
+	st.mu.Lock()
+	defer st.mu.Unlock()
+	return st.writeChunk(c)
 }
 
-func (fs *FileStore) Version() string {
-	return fs.ver
+func (st *FileStore) Version() string {
+	return st.ver
 }
 
-func (fs *FileStore) Rebase(ctx context.Context) error {
+func (st *FileStore) Rebase(ctx context.Context) error {
 	return nil
 }
 
-func (fs *FileStore) Root(ctx context.Context) (hash.Hash, error) {
-	return fs.root, nil
+func (st *FileStore) Root(ctx context.Context) (hash.Hash, error) {
+	st.mu.RLock()
+	defer st.mu.RUnlock()
+	return st.root, nil
 }
 
-func (fs *FileStore) Commit(ctx context.Context, current, last hash.Hash) (bool, error) {
-	fs.mu.Lock()
-	defer fs.mu.Unlock()
+func (st *FileStore) Commit(ctx context.Context, current, last hash.Hash) (bool, error) {
+	st.mu.Lock()
+	defer st.mu.Unlock()
 
-	if fs.root != last {
-		return false, fmt.Errorf("hashes not equal %v != %v", fs.root, last)
+	if st.root != last {
+		return false, fmt.Errorf("hashes not equal %v != %v", st.root, last)
 	}
 
-	if err := writeRootUpdateRecord(fs.wr, current); err != nil {
+	if err := st.writeRootUpdate(current); err != nil {
 		return false, err
 	}
-	if err := fs.wr.Flush(); err != nil {
+	if err := st.wr.Flush(); err != nil {
 		return false, err
 	}
-	fs.root = current
+	st.root = current
 
 	return true, nil
 }
 
-func (fs *FileStore) Stats() interface{} {
+func (st *FileStore) Stats() interface{} {
 	return nil
 }
 
-func (fs *FileStore) StatsSummary() string {
+func (st *FileStore) StatsSummary() string {
 	return ""
 }
 
-func (fs *FileStore) Close() error {
-	return nil
+func (st *FileStore) Close() error {
+	return st.f.Close()
 }
 
-func (fs *FileStore) readChunk(e indexEntry) (Chunk, error) {
+func (st *FileStore) readChunk(e indexEntry) (Chunk, error) {
 	// we must flush before we can read
-	if err := fs.wr.Flush(); err != nil {
+	if err := st.wr.Flush(); err != nil {
 		return Chunk{}, err
 	}
 
-	r, err := readChunkRecord(fs.rd, e)
+	r, err := readChunkRecord(st.f, e)
 	if err != nil {
 		return Chunk{}, err
 	}
@@ -199,26 +208,39 @@ func (fs *FileStore) readChunk(e indexEntry) (Chunk, error) {
 	return NewChunkWithHash(r.key, r.data), nil
 }
 
-func (fs *FileStore) writeChunk(c Chunk) error {
+func (st *FileStore) writeChunk(c Chunk) error {
 	rec := chunkRecord{
 		key:    c.Hash(),
-		root:   fs.root,
+		root:   st.root,
 		length: uint32(c.Size()),
 		data:   c.Data(),
 	}
-	off := uint32(fs.pos)
+	off := uint32(st.pos)
 
-	n, err := writeChunkRecord(fs.wr, rec)
+	n, err := writeChunkRecord(st.wr, rec)
 	if err != nil {
 		return err
 	}
-	fs.pos += int64(n)
+	st.pos += int64(n)
 
-	fs.idx[rec.key] = indexEntry{
+	st.idx[rec.key] = indexEntry{
 		key:    rec.key,
 		length: rec.length,
 		offset: off,
 	}
+	return nil
+}
+
+func (st *FileStore) writeRootUpdate(root hash.Hash) error {
+	n, err := writeChunkRecord(st.wr, chunkRecord{
+		key:  root,
+		root: root,
+	})
+	if err != nil {
+		return err
+	}
+
+	st.pos += int64(n)
 	return nil
 }
 
@@ -231,14 +253,14 @@ type chunkRecord struct {
 	data   []byte
 }
 
-func (r chunkRecord) rootUpdateRecord() bool {
+func (r chunkRecord) isRootUpdateRecord() bool {
 	return r.key == r.root && r.length == 0
 }
 
-func populateFileStoreIndex(fs *FileStore) error {
+func populateFileStoreIndex(store *FileStore) error {
 	var head [chunkRecordHeaderSz]byte
 	for {
-		n, err := fs.rd.ReadAt(head[:], fs.pos)
+		n, err := store.f.ReadAt(head[:], store.pos)
 		if err == io.EOF {
 			return nil
 		}
@@ -252,15 +274,15 @@ func populateFileStoreIndex(fs *FileStore) error {
 		rec := readChunkRecordHeader(head[:])
 
 		// advance to the next record
-		offset := uint32(fs.pos)
-		fs.pos += int64(rec.length + chunkRecordHeaderSz)
+		offset := uint32(store.pos)
+		store.pos += int64(rec.length + chunkRecordHeaderSz)
 
-		if rec.rootUpdateRecord() {
-			fs.root = rec.root
+		if rec.isRootUpdateRecord() {
+			store.root = rec.root
 			continue
 		}
 
-		fs.idx[rec.key] = indexEntry{
+		store.idx[rec.key] = indexEntry{
 			key:    rec.key,
 			length: rec.length,
 			offset: offset,
@@ -295,14 +317,6 @@ func writeChunkRecord(wr io.Writer, rec chunkRecord) (n int, err error) {
 	return
 }
 
-func writeRootUpdateRecord(wr io.Writer, root hash.Hash) (err error) {
-	_, err = writeChunkRecord(wr, chunkRecord{
-		key:  root,
-		root: root,
-	})
-	return err
-}
-
 func readChunkRecord(rd io.ReaderAt, e indexEntry) (r chunkRecord, err error) {
 	buf := make([]byte, e.length+chunkRecordHeaderSz)
 
@@ -311,8 +325,8 @@ func readChunkRecord(rd io.ReaderAt, e indexEntry) (r chunkRecord, err error) {
 	if err != nil {
 		return r, err
 	}
-	if n < len(buf) {
-		return r, fmt.Errorf("too few bytes read (%d vs %d)", n, len(buf))
+	if n != len(buf) {
+		return r, io.ErrShortWrite
 	}
 
 	r = readChunkRecordHeader(buf)
@@ -334,6 +348,11 @@ func readChunkRecordHeader(buf []byte) chunkRecord {
 }
 
 func validateChunkRead(e indexEntry, r chunkRecord) error {
-	// todo(andy)
+	ok := e.length == r.length &&
+		int(e.length) == len(r.data) &&
+		e.key == r.key
+	if !ok {
+		panic("expected ok")
+	}
 	return nil
 }
